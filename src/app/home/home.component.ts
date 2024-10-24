3cfc3733940d97c98f5fdfe6913bef6ef0538781
Ad15C <ad15canon@hotmail.com>
2024-10-24 14:38:57 +0200

    section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Artisan } from '../artisan.model';
+import { ArtisansService } from '../artisans.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
+export class HomeComponent implements OnInit {
+  artisans: Artisan[] = [];
+  topArtisans: Artisan[] = [];
+
+  constructor(private artisansService: ArtisansService) {}
+
+  ngOnInit() {
+    this.artisans = this.artisansService.getArtisans(); // Récupérer les artisans avec les images
+    this.topArtisans = this.artisansService.getTop3Artisans(); // Récupérer les 3 meilleurs artisans
+    console.log(this.topArtisans);
+  }
 }
