536f590dcf0ddb7bdf9e04e6af23afb4670ef2b0
Ad15C <ad15canon@hotmail.com>
2024-11-05 11:04:41 +0100

    tsconfig

---
+++ src/polyfills.ts
@@ -0,0 +1,9 @@
+/***************************************************************************************************
+ * Zone JS est requis par Angular elle-même.
+ */
+import 'zone.js';  // Importez la bibliothèque Zone.js, essentielle pour Angular.
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ * Vous pouvez ajouter ici des polyfills spécifiques à votre application si nécessaire.
+ */
